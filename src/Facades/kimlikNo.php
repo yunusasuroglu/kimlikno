a519407776ea03b6d7b9f572400d65b2f816220c
Yunus Emre <palitorch24@gmail.com>
2024-12-11 22:19:51 +0300

    Update kimlikNo.php

--- src/Facades/kimlikNo.php
+++ src/Facades/kimlikNo.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace yunusasuroglu\KimlikNo\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class KimlikNo extends Facade
+{
+    /**
+     * Facade için sağlanan servis adını döndürür.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'kimlikno';
+    }
+}
