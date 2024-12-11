0beee4d43fd57b00e3cf25e09b0c87e27863fdd1
Yunus Emre <palitorch24@gmail.com>
2024-12-11 22:17:54 +0300

    Update KimlikNo.php

--- src/KimlikNo.php
+++ src/KimlikNo.php
@@ -1,23 +1,41 @@
 <?php
 
-namespace TCKimlik;
+namespace yunusasuroglu\KimlikNo;
 
-class TCKimlik
+class KimlikNo
 {
-    public function validate($tckn)
+    /**
+     * TC Kimlik No doğrulama işlemi.
+     *
+     * @param  string  $tcKimlikNo
+     * @return bool
+     */
+    public static function doğrula($tcKimlikNo)
     {
-        if (!is_numeric($tckn) || strlen($tckn) !== 11) {
+        // Kimlik No'nun 11 karakterden oluşması gerekir.
+        if (strlen($tcKimlikNo) != 11) {
             return false;
         }
-        
-        $digits = str_split($tckn);
-        $checksum1 = (
-            (7 * ($digits[0] + $digits[2] + $digits[4] + $digits[6] + $digits[8])) -
-            ($digits[1] + $digits[3] + $digits[5] + $digits[7])
-        ) % 10;
 
-        $checksum2 = array_sum(array_slice($digits, 0, 10)) % 10;
+        // Kimlik No'nun ilk rakamı sıfır olamaz.
+        if ($tcKimlikNo[0] == '0') {
+            return false;
+        }
 
-        return $checksum1 == $digits[9] && $checksum2 == $digits[10];
+        // İlk 9 rakama göre hesaplama
+        $dizi = str_split($tcKimlikNo);
+        $toplam1 = $dizi[0] + $dizi[2] + $dizi[4] + $dizi[6] + $dizi[8];
+        $toplam2 = $dizi[1] + $dizi[3] + $dizi[5] + $dizi[7];
+        $onuncuHane = $toplam1 * 7 - $toplam2;
+        $onuncuHane = $onuncuHane % 10;
+
+        // 11. hane kontrolü
+        $onbirinciHane = 0;
+        for ($i = 0; $i < 10; $i++) {
+            $onbirinciHane += $dizi[$i];
+        }
+        $onbirinciHane = $onbirinciHane % 10;
+
+        return $onuncuHane == $dizi[9] && $onbirinciHane == $dizi[10];
     }
 }
