8a4546346e67ee1a97e51050e0ee27c57175f5d5
Yunus Emre <palitorch24@gmail.com>
2024-12-11 22:19:26 +0300

    Update KimlikNo.php

--- src/KimlikNo.php
+++ src/KimlikNo.php
@@ -10,32 +10,25 @@ class KimlikNo
      * @param  string  $tcKimlikNo
      * @return bool
      */
-    public static function doğrula($tcKimlikNo)
+    public static function doğrula(string $tcKimlikNo): bool
     {
-        // Kimlik No'nun 11 karakterden oluşması gerekir.
-        if (strlen($tcKimlikNo) != 11) {
+        if (strlen($tcKimlikNo) !== 11) {
             return false;
         }
 
-        // Kimlik No'nun ilk rakamı sıfır olamaz.
-        if ($tcKimlikNo[0] == '0') {
+        if ($tcKimlikNo[0] === '0') {
             return false;
         }
 
-        // İlk 9 rakama göre hesaplama
         $dizi = str_split($tcKimlikNo);
         $toplam1 = $dizi[0] + $dizi[2] + $dizi[4] + $dizi[6] + $dizi[8];
         $toplam2 = $dizi[1] + $dizi[3] + $dizi[5] + $dizi[7];
-        $onuncuHane = $toplam1 * 7 - $toplam2;
+        $onuncuHane = ($toplam1 * 7) - $toplam2;
         $onuncuHane = $onuncuHane % 10;
 
         // 11. hane kontrolü
-        $onbirinciHane = 0;
-        for ($i = 0; $i < 10; $i++) {
-            $onbirinciHane += $dizi[$i];
-        }
-        $onbirinciHane = $onbirinciHane % 10;
+        $onbirinciHane = array_sum(array_slice($dizi, 0, 10)) % 10;
 
-        return $onuncuHane == $dizi[9] && $onbirinciHane == $dizi[10];
+        return $onuncuHane === (int)$dizi[9] && $onbirinciHane === (int)$dizi[10];
     }
 }
