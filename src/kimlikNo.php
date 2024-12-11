5b303f677cd8055edf060707e7fa74bec8605b64
Yunus Emre <palitorch24@gmail.com>
2024-12-11 21:59:01 +0300

    Create kimlikNo.php

---
+++ src/kimlikNo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TCKimlik\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class KimlikNo extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'KimlikNo';
+    }
+}
